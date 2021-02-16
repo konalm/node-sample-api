d966cbc8bc2d3cdc1a1d42ed5a557c8ea61c9992
Connor <Connor@UKMANMAC203.local>
2021-02-16 13:25:25 +0000

    testing github actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Connor');
+    res.send('Hello Connor. Lets test github actions');
 });
 
 app.listen(port);
