8d2434430d1f3570dbdd167024d9838d17967409
Connor <Connor@UKMANMAC203.local>
2021-04-17 17:00:30 +0100

    deploy test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Connor. Lets test github actions - V#2');
+    res.send('Hello Connor. Lets test github actions - V#3 Auto deployment worked ??');
 });
 
 app.listen(port);
