d902fdb1d29b090d99c9881d0b54e703583d4b69
Connor <Connor@UKMANMAC203.local>
2021-04-18 16:48:20 +0100

    cors

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express()
 const port = 8000;
 
-app.get('/', (req, res) => {
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions));
+
+app.get('/', cors(), (req, res) => {
     res.send('Hello Connor. Lets test github actions - V#3 Auto deployment worked ??');
 });
+
+app.get('/name', (req, res) => {
+    res.send('name');
+})
 
 app.listen(port);
 console.log(`App listening on port ${port}`);
